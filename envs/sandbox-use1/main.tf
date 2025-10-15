96f804caa0f1974bc614d8a79ba173865d977de6
Andre Silva <andreswebs@pm.me>
2025-10-15 18:25:51 -0300

    refactor: apps
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- envs/sandbox-use1/main.tf
+++ envs/sandbox-use1/main.tf
@@ -1,1 +1,6 @@
 
+module "zammad" {
+  source             = "../../apps/zammad"
+  name               = var.zammad_app_name
+  private_subnet_ids = var.private_subnet_ids
+}
--- envs/sandbox-use1/variables.tf
+++ envs/sandbox-use1/variables.tf
@@ -1,1 +1,12 @@
+variable "zammad_app_name" {
+  type = string
+}
 
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "List of private subnet IDs"
+}
+
+# variable "domain_name" {
+#   type = string
+# }
