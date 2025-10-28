a29fedb04315d7213e8b88e1c3fba3844824d378
Andre Silva <andreswebs@pm.me>
2025-10-27 23:43:41 -0300

    zammad services
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- apps/zammad/service-zammad-scheduler.tf
+++ apps/zammad/service-zammad-scheduler.tf
@@ -22,3 +22,56 @@ locals {
     }
   }
 }
+
+resource "aws_ecs_task_definition" "zammad_scheduler" {
+  family                   = local.zammad_scheduler_container_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = module.ecs_iam_zammad.role.execution.arn
+  task_role_arn            = module.ecs_iam_zammad.role.task.arn
+  container_definitions    = jsonencode([local.zammad_scheduler_container])
+
+  cpu    = 512
+  memory = 1024
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = var.task_architecture
+  }
+
+  volume {
+    name = local.zammad_storage_volume_name
+
+    efs_volume_configuration {
+      file_system_id     = module.efs.file_system.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+      authorization_config {
+        iam = "ENABLED"
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ecs_service" "zammad_scheduler" {
+  name            = local.zammad_scheduler_container_name
+  cluster         = module.ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.zammad_scheduler.arn
+
+  enable_ecs_managed_tags = true
+  enable_execute_command  = true
+
+  scheduling_strategy = "REPLICA"
+  desired_count       = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets         = var.private_subnet_ids
+    security_groups = [aws_security_group.backend.id]
+  }
+
+  tags = var.tags
+}
--- apps/zammad/service-zammad-server.tf
+++ apps/zammad/service-zammad-server.tf
@@ -1,7 +1,29 @@
 locals {
+  zammad_websocket_container_name   = "zammad-websocket"
   zammad_railsserver_container_name = "zammad-railsserver"
   zammad_nginx_container_name       = "zammad-nginx"
   zammad_nginx_port                 = 8080
+
+  zammad_websocket_container = {
+    name        = local.zammad_websocket_container_name
+    image       = local.zammad_image
+    environment = local.zammad_env
+    mountPoints = local.zammad_volume_mounts
+    healthCheck = local.zammad_healthcheck
+    command     = ["zammad-websocket"]
+
+    essential         = true
+    memoryReservation = 512
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-region        = local.region
+        awslogs-group         = local.log_group_name
+        awslogs-stream-prefix = local.zammad_websocket_container_name
+      }
+    }
+  }
 
   zammad_railsserver_container = {
     name        = local.zammad_railsserver_container_name
@@ -31,12 +53,12 @@ locals {
     command     = ["zammad-nginx"]
 
     essential         = true
-    memoryReservation = 512
+    memoryReservation = 256
 
     dependsOn = [
       {
-        name      = local.zammad_railsserver_container_name
-        condition = "HEALTHY"
+        containerName = local.zammad_railsserver_container_name
+        condition     = "HEALTHY"
       },
     ]
 
@@ -57,4 +79,61 @@ locals {
       }
     }
   }
+}
+
+resource "aws_ecs_task_definition" "zammad_server" {
+  family                   = "zammad-server"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = module.ecs_iam_zammad.role.execution.arn
+  task_role_arn            = module.ecs_iam_zammad.role.task.arn
+  container_definitions = jsonencode([
+    local.zammad_websocket_container,
+    local.zammad_railsserver_container,
+    local.zammad_nginx_container,
+  ])
+
+  cpu    = 1024
+  memory = 2048
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = var.task_architecture
+  }
+
+  volume {
+    name = local.zammad_storage_volume_name
+
+    efs_volume_configuration {
+      file_system_id     = module.efs.file_system.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+      authorization_config {
+        iam = "ENABLED"
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ecs_service" "zammad_server" {
+  name            = "zammad-server"
+  cluster         = module.ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.zammad_server.arn
+
+  enable_ecs_managed_tags = true
+  enable_execute_command  = true
+
+  scheduling_strategy = "REPLICA"
+  desired_count       = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets         = var.private_subnet_ids
+    security_groups = [aws_security_group.backend.id]
+  }
+
+  tags = var.tags
 }
